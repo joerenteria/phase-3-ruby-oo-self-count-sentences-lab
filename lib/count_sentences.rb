ff29798b6ee0f74c3669d9179f36534500c8e2d2
Joe Renteria <joe@electronflight.com>
2021-09-03 19:23:59 -0500

    commit tree 0191e06239a68204db3e76f271f9de8ad2a07c2e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+      else
+        return false
+    end
 
   end
 
